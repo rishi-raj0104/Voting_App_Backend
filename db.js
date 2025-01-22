a57669299443f14fcc1b10bb041808f1532d9741
Rishi Raj <rishi.rishiraj12@gmail.com>
2025-01-22 23:26:26 +0530

    first commit

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 mongoose.connect(process.env.DB_URL,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
+    //useNewUrlParser:true,
+    //useUnifiedTopology:true,
 })
 .then(() => console.log('Connected to MongoDB'))
 .catch((err) => console.error('Connection error', err));
